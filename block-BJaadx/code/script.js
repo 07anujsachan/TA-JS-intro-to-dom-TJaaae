7ade01d7b554d31cb81652f9ae09b1a5208e94df
Anuj Sachan <anujsachan@Anujs-MacBook-Pro.local>
2023-01-18 14:21:19 +0530

    Assignment: Selecting Elements

--- block-BJaadx/code/script.js
+++ block-BJaadx/code/script.js
@@ -1,14 +1,15 @@
 // 1. Change the title of the page to `Hello AltCampus!`
-
+document.title = " Hello AltCampus"
 // 2. Select the element using the children property:
 
 //    - Select the `h1` element and change the value to `Learning DOM`
-
+ document.body.children[0].innerText = 'Learning DOM'
 //    - Select the first `li` element inside the `ul` with class `topics` and change the innerText to `all about document`
+  document.body.querySelector('.topics').children[0].innerText = ' all about document'
 //    - Select the input element with name `email`
-
+ let emailElm = document.body.querySelector('label').children[0]
 // 3. Log the number (using console.log) of children of all the `li` element inside the ul with class `topics`
-
+console.log(document.body.querySelector('.topics').getElementsByTagName('li'));
 // 4. Select the first input using the `type` selector and store them in variable named `emailInput`
 
 // 5. Select the ul element using class selector and store in `topics`
